039db4dd974998dcae09b38f94523b7d84633162
Rene Cosme <rvbcosme@gmail.com>
2025-11-07 21:10:05 +0800

    Update ApiConstants.kt

--- app/src/main/java/com/example/eyedtrack/api/ApiConstants.kt
+++ app/src/main/java/com/example/eyedtrack/api/ApiConstants.kt
@@ -2,7 +2,7 @@ package com.example.eyedtrack.api
 
 object ApiConstants {
     // Use 10.0.2.2 for Android Emulator to connect to host machine's localhost
-    const val BASE_URL = "http://192.168.68.110:5000/"
+    const val BASE_URL = "http://<your-private-IP-address>:<port>/"
 
     // API Endpoints
     const val PROCESS_FRAME = "process_frame"
